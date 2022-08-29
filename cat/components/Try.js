63d0be1a12e35228e66ad19dd097fa86a39cc913
KutembaChinambu <kutembachinambu@rocketmail.com>
2022-08-29 20:04:23 +0200

    Added cat app

--- cat/components/Try.js
+++ cat/components/Try.js
@@ -1,44 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 
 export default function Try() {
-  let [quote, setQuote] = React.useState('')
-  let [source, setSource] = React.useState('')
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+    console.log(data);
 
-  const fetchApiCall = () => {
-    fetch("https://api.thecatapi.com/v1/breeds", {
-      "method": "GET",
-      "headers": {
-        "x-api-key": "94831cc8-7673-472c-9312-8ac6729d300a"
+
+    useEffect(() => {
+        fetch("https://api.thecatapi.com/v1/breeds", {
+             "method": "GET",
+             "headers": {
+             "x-api-key": "94831cc8-7673-472c-9312-8ac6729d300a"
       }
     })
-      .then(response => response.json())
-      .then(response => {
-        setQuote(response.content);
-        setSource(response.originator.name)
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+        
+        .then((response) => response.json())
+          .then((json) => setData(json))
+          .catch((error) => console.error(error))
+          .finally(() => setLoading(false));
+      }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Cats API</Text>
-      <Text>Click to fetch records</Text>
-      <TouchableHighlight onPress={fetchApiCall}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>Load Cats</Text>
-        </View>
-      </TouchableHighlight>
-      <View>
-        <Text>{id}</Text>
-        <Text>{source}</Text>
-     
+    <View style={{ flex: 1, padding: 24 }}>
+    {isLoading ? <Text>Loading...</Text> : 
+    ( <View style={{ flex: 1, flexDirection: 'column', justifyContent:  'space-between'}}>
+        <Text style={{ fontSize: 18, color: 'green', textAlign: 'center'}}>{data.name}</Text>
+        <Text style={{ fontSize: 14, color: 'green', textAlign: 'center', paddingBottom: 10}}>Cats:</Text>
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+
+            <Text>{item.name + ' ' + item.image}</Text>
+            
+
+    
+           
+          )}
+        />
       </View>
-      <StatusBar style="auto" />
-    </View>
+    )}
+  </View>
   );
 }
 const styles = StyleSheet.create({
